a3b37cfd12d43367b2cac277b68e3d648083e911
GuFariaszx <zacharingustavo@gmail.com>
2024-05-02 17:02:44 -0300

    feat: Exemplo

---
+++ exemplo.js
@@ -0,0 +1,21 @@
+// Escreva um programa que solicite o nome do usuário e lhe dê boas vindas,
+// exibindo uma mensagem pelo cosole 
+
+// prompt -> Recebe uma entrada digitada
+let nome = prompt("Olá, Digite seu Nome: ")
+
+// Concatenação
+console.log("Seja bem vindo " + nome + "!!!");
+
+let pontos = 100
+
+// let bonus = parseInt( prompt ("Quntos pontos você deseja somar?") )
+
+let pontuacaoTotal = pontos + bonus
+console.log(pontuacaoTotal)
+
+//parseFloat() -> Transforma o que está entre () em número com, (quebrado )
+
+let notaAluno = parseFloat(prompt("Digite a nota do aluno: "))
+
+console.log(notaAluno)
