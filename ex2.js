8f14a478442d0e7e67ead90b494e67e655aeaa76
GuFariaszx <zacharingustavo@gmail.com>
2024-05-03 15:18:38 -0300

    feat: Exercício 2 feito

---
+++ ex2.js
@@ -0,0 +1,30 @@
+// Exercício 2: Verificação de dia da semana
+// Escreva um programa que solicita ao usuário um número de 1 a 7 representando um dia da
+// semana e exibe o nome correspondente a esse dia. Considere que 1 representa segunda-feira,
+// 2 representa terça-feira e assim por diante.
+
+let day = prompt ("Digite um número de 1 a 7")
+
+switch (day) {
+    case "1":
+        prompt("Você escolheu Segunda Feira")
+        break
+    case "2":
+        prompt("Você escolheu Terça Feira")
+        break
+    case "3":
+        prompt("Você escolheu Quarta Feira")
+        break
+    case "4":
+        prompt("Você escolheu Quinta Feira")
+        break
+    case "5":
+        prompt("Você escolheu Sexta Feira")
+        break
+    case "6":
+        prompt("Você escolheu Sábado")
+        break
+    case "7":
+        prompt("Você escolheu Domingo")
+        break
+}
