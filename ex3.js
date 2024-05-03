35bd4f24d40598826dfba3ff25e4715970e4412e
GuFariaszx <zacharingustavo@gmail.com>
2024-05-03 15:20:42 -0300

    feat:Exercício 3 feito

---
+++ ex3.js
@@ -0,0 +1,11 @@
+// Exercício 3: Ímpar ou par
+// Faça um programa que receba um número do usuário e informe se este número é par ou
+// ímpar.
+
+let ParouImpar = prompt ("Digite um número:") 
+
+if (ParouImpar % 2 === 0) {
+console.log("Par")
+} else {
+console.log ("Impar")
+}
