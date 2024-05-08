56249d97ed9413166285e4b2ba15d603ca1d0b3f
GuFariaszx <zacharingustavo@gmail.com>
2024-05-08 15:22:13 -0300

    feat: Exercício Loop 10 Feito

---
+++ loop/ex10loop.js
@@ -0,0 +1,12 @@
+// 10. Faça um programa que receba um número do usuário e continue
+// a pedir o número se o numero for diferente de 0.
+
+let dif = 1
+
+while (dif != 0) {
+    dif = parseInt(prompt("Informe um número: "))
+}
+
+console.log("Boaaaa, Você Acertouu!!")
+
+
