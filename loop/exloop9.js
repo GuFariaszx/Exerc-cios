55fcfbc945595e6f08336a16980e11159081efd3
GuFariaszx <zacharingustavo@gmail.com>
2024-05-10 15:04:37 -0300

    feat: Exercício Loop 9 Feito

--- loop/exloop9.js
+++ loop/exloop9.js
@@ -1,7 +1,7 @@
 // 9. Faça um programa que receba um número do usuário e mostre
 // no console a contagem regressiva até 0.
 
-let reg = prompt("Informe um número: ")
+let reg = prompt("Informe um número Inteiro qualquer: ")
 
 while (reg >= 0) {
     console.log(reg)
