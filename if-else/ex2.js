ca76f35842ac7e44ae94fd91a31cf77eca0baaa6
GuFariaszx <zacharingustavo@gmail.com>
2024-05-10 14:49:02 -0300

    feat:Exercício 2 Feito

--- if-else/ex2.js
+++ if-else/ex2.js
@@ -7,24 +7,24 @@ let day = prompt ("Digite um número de 1 a 7")
 
 switch (day) {
     case "1":
-        prompt("Você escolheu Segunda Feira")
+        prompt("Você escolheu Segunda Feira!")
         break
     case "2":
-        prompt("Você escolheu Terça Feira")
+        prompt("Você escolheu Terça Feira!")
         break
     case "3":
-        prompt("Você escolheu Quarta Feira")
+        prompt("Você escolheu Quarta Feira!")
         break
     case "4":
-        prompt("Você escolheu Quinta Feira")
+        prompt("Você escolheu Quinta Feira!")
         break
     case "5":
-        prompt("Você escolheu Sexta Feira")
+        prompt("Você escolheu Sexta Feira!")
         break
     case "6":
-        prompt("Você escolheu Sábado")
+        prompt("Você escolheu Sábado!")
         break
     case "7":
-        prompt("Você escolheu Domingo")
+        prompt("Você escolheu Domingo!")
         break
 }
