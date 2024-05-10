fe56902084551d2481e9d91b3beaa7b1b0e2f490
GuFariaszx <zacharingustavo@gmail.com>
2024-05-10 14:53:18 -0300

    feat: Exercício 4 Feito

--- if-else/ex4.js
+++ if-else/ex4.js
@@ -3,10 +3,10 @@
 // notas e a mensagem de APROVADO para média superior ou igual a 7,0 RECUPERAÇÃO para
 // notas entre 5.0 e 7,0 ou a mensagem de REPROVADO para média inferior a 5,0.
 
-let no1 = parseFloat(prompt ("Digite a primeira nota de Gustavo de 1 à 10:"))
-let no2 = parseFloat(prompt ("Digite a segunda nota de Gustavo de 1 à 10:"))
-let no3 = parseFloat(prompt ("Digite a terceira nota de GUstavo de 1 à 10:"))
-let no4 = parseFloat(prompt ("Digite a quarta nota de Gustavo de 1 à 10:"))
+let no1 = parseFloat(prompt ("Digite a primeira nota de Jorge de 1 à 10:"))
+let no2 = parseFloat(prompt ("Digite a segunda nota de Jorge de 1 à 10:"))
+let no3 = parseFloat(prompt ("Digite a terceira nota de Jorge de 1 à 10:"))
+let no4 = parseFloat(prompt ("Digite a quarta nota de Jorge de 1 à 10:"))
 
 let calc = (no1 + no2 + no3 + no4) / 4 
 
